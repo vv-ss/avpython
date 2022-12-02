44a80ba6a414c8a5bb230344e6cb63a8d266155e
av <vasu.singh@gmail.com>
2022-12-02 20:29:29 +0100

    Added questions to board

--- jugend_forscht/tictactoe/tic tac toe.py
+++ jugend_forscht/tictactoe/tic tac toe.py
@@ -1,32 +1,37 @@
 # Mit 'import random' öffnen wir die Bücherrei 'random', da wir sie später brauchen werden.
 import random
+
 Wert_dict = {}
 com_move_first = True
 
+# Welche Zuege kann der Spieler ziehen
 def next_players_state(n):
-   moves=[]
-   for i in range(0, len(n)):
-       if n[i]=='0':
-           x = list(n)
-           x[i]='2'
-           moves.append(''.join(x))
-   return moves
-#TODO: states change, end_state change,
-def next_com_state(n):
-   next_states=[]
-   for i in range(0, len(n)):
-       if n[i]=='0':
-           x = list(n)
-           x[i]='1'
-           next_states.append(''.join(x))
-   return next_states
+    moves = []
+    for i in range(0, len(n)):
+        if n[i] == '0':
+            x = list(n)
+            x[i] = '2'
+            moves.append(''.join(x))
+    return moves
 
+# Welche Zuege kann der Computer ziehen
+def next_com_state(n):
+    next_states = []
+    for i in range(0, len(n)):
+        if n[i] == '0':
+            x = list(n)
+            x[i] = '1'
+            next_states.append(''.join(x))
+    return next_states
+
+# Unentschieden
 def draw(n):
     if is_player_end_state(n) or is_com_end_state(n) or '0' in n:
         return False
     else:
         return True
 
+# Positionen, wie die Lage aussehen koennte, wenn der Spieler gewinnt
 def is_player_end_state(n):
     if len(n) == 9:
         if n[0] == n[3] == n[6] == '2':
@@ -68,36 +73,39 @@ def is_player_end_state(n):
             player_win = False
     return player_win
 
+
+# Positionen, wie die Lage aussehen koennte, wenn der Computer gewinnt
 def is_com_end_state(n):
-    if n[0]==n[3]==n[6]=='1':
+    if n[0] == n[3] == n[6] == '1':
         computer_win = True
-    elif n[1]==n[4]==n[7]=='1':
+    elif n[1] == n[4] == n[7] == '1':
         computer_win = True
-    elif n[2]==n[5]==n[8]=='1':
+    elif n[2] == n[5] == n[8] == '1':
         computer_win = True
-    elif n[0]==n[1]==n[2]=='1':
+    elif n[0] == n[1] == n[2] == '1':
         computer_win = True
-    elif n[3]==n[4]==n[5]=='1':
+    elif n[3] == n[4] == n[5] == '1':
         computer_win = True
-    elif n[6]==n[7]==n[8]=='1':
+    elif n[6] == n[7] == n[8] == '1':
         computer_win = True
-    elif n[0]==n[4]==n[8]=='1':
+    elif n[0] == n[4] == n[8] == '1':
         computer_win = True
-    elif n[2]==n[4]==n[6]=='1':
+    elif n[2] == n[4] == n[6] == '1':
         computer_win = True
     else:
         computer_win = False
     return computer_win
 
+# Ist der Computer dran
 def com_turn(state):
-    X=0
-    O=0
+    X = 0
+    O = 0
     for i in state:
-        if i=='1':
-            O+=1
-        if i=='2':
-            X+=1
-    if O==X:
+        if i == '1':
+            O += 1
+        if i == '2':
+            X += 1
+    if O == X:
         if com_move_first:
             return True
         else:
@@ -108,6 +116,7 @@ def com_turn(state):
         else:
             return False
 
+#
 def get_wert(state):
     if state in Wert_dict:
         return Wert_dict[state]
@@ -117,7 +126,7 @@ def get_wert(state):
         return -100
     if draw(state):
         return 0
-    alle_werte=[]
+    alle_werte = []
     state_wert = 0
     if com_turn(state):
         for i in next_com_state(state):
@@ -130,6 +139,7 @@ def get_wert(state):
     Wert_dict[state] = state_wert
     return state_wert
 
+
 def com_move(state):
     next_states = next_com_state(state)
     next_best_state = next_states[0]
@@ -139,17 +149,19 @@ def com_move(state):
         if wert > next_best_wert:
             next_best_wert = wert
             next_best_state = i
-    for i in range(0,9):
+    for i in range(0, 9):
         if state[i] != next_best_state[i]:
             return i
 
+
 def com_random_move(state):
     while True:
-        x = random.randint(0,8)
+        x = random.randint(0, 8)
         if state[x] == '0':
             return x
 
-def convert_state_to_board(state,i):
+
+def convert_state_to_board(state, i):
     if state[i] == '0':
         return ' '
     if state[i] == '1':
@@ -157,14 +169,19 @@ def convert_state_to_board(state,i):
     if state[i] == '2':
         return 'X'
 
+
 def printBoard3(state):
     print('__ __ __ __ __')
-    print('|',convert_state_to_board(state,0),'|',convert_state_to_board(state,1),'|',convert_state_to_board(state,2),'|')
+    print('|', convert_state_to_board(state, 0), '|', convert_state_to_board(state, 1), '|',
+          convert_state_to_board(state, 2), '|')
     print('__ __ __ __ __')
-    print('|',convert_state_to_board(state,3),'|',convert_state_to_board(state,4),'|',convert_state_to_board(state,5),'|')
+    print('|', convert_state_to_board(state, 3), '|', convert_state_to_board(state, 4), '|',
+          convert_state_to_board(state, 5), '|')
     print('__ __ __ __ __')
-    print('|',convert_state_to_board(state,6),'|',convert_state_to_board(state,7),'|',convert_state_to_board(state,8),'|')
+    print('|', convert_state_to_board(state, 6), '|', convert_state_to_board(state, 7), '|',
+          convert_state_to_board(state, 8), '|')
     print('__ __ __ __ __')
+
 
 def printBoard4(state):
     print('__ __ __ __ __ __')
@@ -183,11 +200,11 @@ def printBoard4(state):
 
 def game():
     global com_move_first
-    brett_size=input('What size do you want? Give in 3,4 or 5.')
-    stufe=input('Do you want an easy or a hard level?')
-    first_move=input('Would you want to start(yes/no)')
-    if first_move=='yes':
-        com_move_first=False
+    brett_size = input('What size do you want? Give in 3,4 or 5.')
+    stufe = input('Do you want an easy or a hard level?')
+    first_move = input('Would you want to start(yes/no)')
+    if first_move == 'yes':
+        com_move_first = False
     # Board zeigen
     state = '000000000'
     while True:
@@ -218,5 +235,6 @@ def game():
             state_list = list(state)
             state_list[int(move)] = '2'
             state = ''.join(state_list)
+
 
 game()
