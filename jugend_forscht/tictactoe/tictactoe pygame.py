3426967cb0f8027ed41ba382e12a141e5cbaa79a
av <vasu.singh@gmail.com>
2022-12-02 19:45:39 +0100

    Added questions to board

--- jugend_forscht/tictactoe/tictactoe pygame.py
+++ jugend_forscht/tictactoe/tictactoe pygame.py
@@ -1,6 +1,6 @@
 import pygame
 from pygame.locals import *
-darkgreen=()
+darkgreen=(0,100,0)
 green = (0, 255, 0)
 tuerkis=(64, 224, 208)
 orange = (255,165,0)
@@ -58,6 +58,7 @@ def user_click():
 
     else:
         row = None
+    print(row, col)
 
 def print_board():
     # Rechteck mitte links
@@ -96,19 +97,92 @@ def print_board():
     pygame.draw.rect(surface, lightblue, pygame.Rect(600, 598, 168, 168))
     pygame.draw.rect(surface, orange, pygame.Rect(594, 594, 176, 176), 10)
 
-print_board()
+
 green_cover=pygame.draw.rect(surface,tuerkis,pygame.Rect(0,0,1000,1000))
-question1=font_style.render('Do you want to play with computer(y/n)?',True,purple)
+
+question1=font_style.render('Do you want to play against easy or hard(e/h)?',True,purple)
 surface.blit(question1, (50,490))
 pygame.display.flip()
+
+hardness_level = None
 while True:
+    if (hardness_level):
+        break
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_e:
+                print("Key E has been pressed")
+                hardness_level = 'Easy'
+                break
+            if event.key == pygame.K_h:
+                hardness_level = 'Hard'
+                print("Key H has been pressed")
+                break
+
+
+answer1=font_style.render("You chose " + hardness_level, True, darkgreen)
+surface.blit(answer1, (50,540))
+pygame.display.flip()
+
+question=font_style.render('Do you want to play on 3x3 or 4x4 (3/4)?',True,purple)
+surface.blit(question, (50,650))
+pygame.display.flip()
+
+board_size = None
+while True:
+    if (board_size):
+        break
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_3:
+                print("Key 3 has been pressed")
+                board_size = '3x3'
+                break
+            if event.key == pygame.K_4:
+                board_size = '4x4'
+                print("Key 4 has been pressed")
+                break
+
+
+answer=font_style.render("You chose " + board_size, True, darkgreen)
+surface.blit(answer, (50,740))
+pygame.display.flip()
+
+
+
+# Ask next question
+question2=font_style.render('Do you want to start(y/n)?',True,purple)
+surface.blit(question2, (50,830))
+pygame.display.flip()
+
+start_player = None
+while True:
+    if (start_player):
+        break
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_y:
                 print("Key Y has been pressed")
+                start_player = 'Y'
                 break
             if event.key == pygame.K_n:
+                start_player = 'N'
                 print("Key N has been pressed")
                 break
+
+print_board()
+pygame.display.flip()
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            user_click()
+        else:
+            pass
