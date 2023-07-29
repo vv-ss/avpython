d83602c2fd2113c4d174e00d2d0fb31090f8a439
av <vasu.singh@gmail.com>
2023-07-29 12:31:33 +0200

    word

--- viyonacode/shapes.py
+++ viyonacode/shapes.py
@@ -1,14 +1,75 @@
-class Rectangle():
+class Rectangle:
     def __init__(self):
-        self.e1 = (400, 400)
-        self.width = (200)
-        self.height = (100)
+        self.x = 400
+        self.y = 400
+        self.width = 200
+        self.height = 100
+
     def move(self, direction):
         if direction == 'UP':
-            self.e1[1] = self.e1[1] + 10
+            self.y -= 10
         if direction == 'DOWN':
-            self.e1[1] = self.e1[1] - 10
+            self.y += 10
         if direction == 'RIGHT':
-            self.e1[0] = self.e1[0] + 10
+            self.x += 10
         if direction == 'LEFT':
-            self.e1[0] = self.e1[0] - 10
+            self.x -= 10
+
+    def covers(self, x, y):
+        if self.x < x < (self.x + self.width) and self.y < y < (self.y + self.height):
+            return True
+        else:
+            return False
+class Triangle:
+    def __init__(self):
+        self.x1 = 400
+        self.y1 = 400
+        self.x2 = 300
+        self.y2 = 500
+        self.x3 = 500
+        self.y3 = 500
+
+
+    def move(self, direction):
+        if direction == 'UP':
+            self.y1 -= 10
+            self.y2 -= 10
+            self.y3 -= 10
+        if direction == 'DOWN':
+            self.y1 += 10
+            self.y2 += 10
+            self.y3 += 10
+        if direction == 'RIGHT':
+            self.x1 += 10
+            self.x2 += 10
+            self.x3 += 10
+        if direction == 'LEFT':
+            self.x1 -= 10
+            self.x2 -= 10
+            self.x3 -= 10
+
+    def covers(self, x, y):
+        return False
+
+class Circle:
+    def __init__(self):
+        self.x = 400
+        self.y = 400
+        self.r = 200
+
+
+    def move(self, direction):
+        if direction == 'UP':
+            self.y -= 10
+        if direction == 'DOWN':
+            self.y += 10
+        if direction == 'RIGHT':
+            self.x += 10
+        if direction == 'LEFT':
+            self.x -= 10
+
+    def covers(self, x, y):
+        if (self.x - x)**2 + (self.y - y)**2 < self.r**2:
+            return True
+        else:
+            return False
--- viyonacode/word.py
+++ viyonacode/word.py
@@ -1,14 +1,13 @@
 import pygame
 from pygame.locals import *
-import * from shapes
+from shapes import *
 
 width = 900
 height = 900
-cellwidth = width/10
+cellwidth = width / 10
 window = pygame.display.set_mode((width, height))
 
-rectwidth = 100
-rectheight = 100
+shapes = []
 
 grey = (230, 230, 230)
 lightblue = (5, 213, 250)
@@ -16,7 +15,6 @@ black = (0, 0, 0)
 white = (255, 255, 255)
 blue = (50, 50, 255)
 lila = (205, 110, 255)
-
 
 pygame.init()
 font1 = pygame.font.SysFont('freesansbold.ttf', 30)
@@ -27,66 +25,91 @@ font3 = pygame.font.SysFont('freesansbold.ttf', 70)
 def draw_user_interface():
     wall = cellwidth
     line_vertical = cellwidth
-    line_horizontal = cellwidth/2
+    line_horizontal = cellwidth / 2
     pygame.display.set_caption('Word by Viyona')
     window.fill(white)
     for lines in range(0, 20):
-        pygame.draw.line(window, grey,(0, line_vertical), (width, line_vertical))
-        line_vertical += cellwidth/2
+        pygame.draw.line(window, grey, (0, line_vertical), (width, line_vertical))
+        line_vertical += cellwidth / 2
     for lines in range(0, 20):
-        pygame.draw.line(window, grey,(line_horizontal, cellwidth), (line_horizontal, height))
-        line_horizontal += cellwidth/2
-    pygame.draw.line(window, lightblue,(0, cellwidth), (width, cellwidth))
+        pygame.draw.line(window, grey, (line_horizontal, cellwidth), (line_horizontal, height))
+        line_horizontal += cellwidth / 2
+    pygame.draw.line(window, lightblue, (0, cellwidth), (width, cellwidth))
     pygame.draw.line(window, lightblue, (0, 0), (width, 0))
     for line in range(0, 10):
         pygame.draw.line(window, lightblue, (wall, 0), (wall, cellwidth))
         wall += cellwidth
     pygame.draw.rect(window, lila, pygame.Rect(0, height - 100, width, 100))
+    print_button('Rect', black, 5, 50, font1)
+    print_button('Circle', black, cellwidth + 20, 50, font1)
+    print_button('Triangle', black, cellwidth * 2 + 20, 50, font1)
+    print_button('+', black, cellwidth * 3 + 20, 28, font3)
+    print_button('-', black, cellwidth * 4 + 20, 28, font3)
+    print_button('Color', black, cellwidth * 5 + 20, 50, font1)
     pygame.display.flip()
 
-def shape(form):
+
+def redraw_shape(shape):
+    if type(shape) == Rectangle:
+        pygame.draw.rect(window, blue, pygame.Rect(shape.dijkstra_solved, shape.y, shape.width, shape.height))
+    if type(shape) == Circle:
+        pygame.draw.circle(window, blue, (shape.dijkstra_solved, shape.y), shape.r)
+    if type(shape) == Triangle:
+        pygame.draw.polygon(window, blue, ((shape.x1, shape.y1), (shape.x2, shape.y2), (shape.x3, shape.y3)))
+    pygame.display.flip()
+
+def create_shape(form):
     if form == 'rect':
+        shape = Rectangle()
+        pygame.draw.rect(window, blue, pygame.Rect(shape.x, shape.y, shape.width, shape.height))
 
-        pygame.draw.rect(window, blue, pygame.Rect(450 - rectwidth/2, 450 - rectheight/2, rectwidth, rectheight))
-    pygame.display.flip()
     if form == 'circle':
-        pygame.draw.circle(window, blue,(450, 450), 100)
-    #if form == 'polygon':
+        shape = Circle()
+        pygame.draw.circle(window, blue, (shape.x, shape.y), shape.r)
+    # if form == 'polygon':
     #    pygame.draw.polygon(window,blue,((600,700),(600,600),(750,550),(700,600),(700,700)))
-    #if form == 'ellipse':
+    # if form == 'ellipse':
     #    pygame.draw.ellipse(window, blue,(550, 550, 600, 550), 650)
+
     if form == 'triangle':
-        pygame.draw.polygon(window,blue,((400,450),(500,450),(450,500)))
+        shape = Triangle()
+        pygame.draw.polygon(window, blue, ((shape.x1, shape.y1), (shape.x2, shape.y2), (shape.x3, shape.y3)))
     pygame.display.flip()
+    shapes.append(shape)
 
-def
 
-
-def print_button(text,farbe,x,y,font):
+def print_button(text, farbe, x, y, font):
     text = font.render(text, True, farbe)
     window.blit(text, (x, y))
     pygame.display.flip()
 
-def button_shape(x,y):
-    if (x,y) == (0, 0):
-        print_button('Rect', black, 5, height - 50, font1)
-        print_button('Triangle', black,cellwidth + 5, height - 50, font1)
-        pygame.display.flip()
+
+def button_shape(x):
+    if x == 0:
+        create_shape('rect')
+    if x == 1:
+        create_shape('circle')
+    if x == 2:
+        create_shape('triangle')
+
+    pygame.display.flip()
 
 
 draw_user_interface()
-shape('triangle')
-print_button('Shape',black,5, 50,font1)
-print_button('Color',black,cellwidth + 20, 50,font1)
-print_button(' + ',black,cellwidth * 2 + 20, 28,font3)
-print_button(' - ',black,cellwidth * 3 + 20, 28,font3)
-print_button('Girth',black,cellwidth * 4 + 20, 50,font1)
-print_button('Ecken',black,cellwidth * 5 + 20, 50,font1)
-print_button('Line',black,cellwidth * 6 + 20, 50,font1)
-print_button(' New ',black,cellwidth * 7 + 5, 50,font1)
-print_button('Back ',black,cellwidth * 8 + 5, 50,font1)
-print_button('Done ',black,cellwidth * 9 + 5, 50,font1)
+
+
 print('came here2')
+
+
+def select_shape(x, y):
+    for i in shapes:
+        print(i)
+        if i.covers(x, y):
+            print('hello')
+            return i
+
+
+s = None
 
 while True:
     for event in pygame.event.get():
@@ -94,19 +117,30 @@ while True:
             pygame.quit()
         if event.type == pygame.MOUSEBUTTONDOWN:
             x, y = pygame.mouse.get_pos()
-            y = y// 100
-            x = x // 100
             print(x, y)
-            button_shape(x,y)
+            if y < cellwidth:
+                button_shape(x // cellwidth)
+            else:
+                print('came here')
+                s = select_shape(x, y)
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
-                rect.move('UP')
-        if event.type == pygame.KEYDOWN:
+                if s is not None:
+                    s.move('UP')
+                    draw_user_interface()
+                    redraw_shape(s)
             if event.key == pygame.K_DOWN:
-                rect.move('DOWN')
-        if event.type == pygame.KEYDOWN:
+                if s is not None:
+                    s.move('DOWN')
+                    draw_user_interface()
+                    redraw_shape(s)
             if event.key == pygame.K_RIGHT:
-                rect.move('RIGHT')
-        if event.type == pygame.KEYDOWN:
+                if s is not None:
+                    s.move('RIGHT')
+                    draw_user_interface()
+                    redraw_shape(s)
             if event.key == pygame.K_LEFT:
-                rect.move('LEFT')
+                if s is not None:
+                    s.move('LEFT')
+                    draw_user_interface()
+                    redraw_shape(s)
